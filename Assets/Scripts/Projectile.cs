c67dfda091ed999c6cea82be4df7df7d7c0a8d4b
Michael Zampogna <57304@cgs.act.edu.au>
2023-04-09 17:28:51 +1000

    shooting part 2

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,18 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed = 5;
+    private float direction;
+    private bool hit;
+    // How long projectile is active
+    private float lifetime;
+
+    private BoxCollider2D boxCollider;
+    private Animator anim;
+
+    private void Awake() 
     {
-        
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+    
+    private void Update() 
+    {
+        if (hit) return;
+        float movementSpeed = speed * Time.deltaTime * direction;
+        transform.Translate(movementSpeed, 0 , 0);
+
+        lifetime += Time.deltaTime;
+        if (lifetime > 5) gameObject.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        hit = true;
+        boxCollider.enabled = false;
+        anim.SetTrigger("explode");
+    }
+
+    public void SetDirection(float _direction) 
+    {
+        lifetime = 0;
+        direction = _direction;
+        gameObject.SetActive(true);
+        hit = false;
+        boxCollider.enabled = true;
+
+        float localScaleX = transform.localScale.x;
+        if (Mathf.Sign(localScaleX) != _direction)
+            localScaleX = -localScaleX;
+
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+
+    }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
     }
 }
